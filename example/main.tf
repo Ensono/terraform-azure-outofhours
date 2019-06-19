3bca8ed7be2d882d133cf689c96742b719fd1e05
William Ayerst <william.ayerst@amido.com>
2019-06-19 15:04:33 +0100

    Enforce Tag Types for OOH Shutdown

--- example/main.tf
+++ example/main.tf
@@ -6,8 +6,9 @@ resource "random_pet" "name" {
   prefix    = "amido-stacks"
 }
 
-module "aks-registry" {
+module "azure-outofhours" {
   source                  = "git::https://github.com/amido/terraform-azure-outofhours.git"
   resource_group_name     = "${random_pet.name.id}"
   resource_group_location = "${var.resource_group_location}"
 }
+ 
--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
-resource "azurerm_policy_definition" "DefaultShutdownTag" {
+resource "azurerm_policy_definition" "AddDefaultOOHShutdownTag" {
   count        = "${var.create_resource ? 1 : 0 }"
   name         = "DefaultShutdownTag"
   policy_type  = "Custom"
@@ -22,5 +22,49 @@ resource "azurerm_policy_definition" "DefaultShutdownTag" {
         }
     }
     POLICY_RULE
+  }
+
+resource "azurerm_policy_definition" "EnforceProperDefaultOOHsShutdownTagValue" {
+  count        = "${var.create_resource ? 1 : 0 }"
+  name         = "DefaultShutdownTag"
+  policy_type  = "Custom"
+  mode         = "Indexed"
+  display_name = "DefaultShutdownTag policy definition"
+
+  policy_rule = <<POLICY_RULE
+    {
+        "if": {
+            "allOf": [{
+                 "field": "tags['outofhours']",
+                 "exists": "true"
+            },
+            {"field": "tags['outofhours']",
+             "notIn": "[parameters('allowedOOHTags')]"
+            }
+            ]
+        },
+        "then": {
+            "effect": "deny",
+        }
+    }
+    POLICY_RULE
+  
+parameters = <<PARAMETERS
+    {
+    "allowedOOHTags": {
+      "type": "Array",
+        "metadata": {
+            "description": "The list of allowed values for for outofhours tag.",
+            "displayName": "Allowed OutofHours Tag values",
+        },
+        "allowedValues": [
+            "shutdown",
+            "scaledown",
+            "delete",
+            "donothing"
+        ]
+    }
+  }
+PARAMETERS
   }
 
